77898a3a54257b2cc60e3d090ea67e75097b4f8d
CaillaudPA <caillaud.pierreantoine@yahoo.com>
2018-02-06 15:57:35 +0100

    Ajout de la liste des personnages sur l'ecran de detail du film

--- src/pages/film/detail/detail.ts
+++ src/pages/film/detail/detail.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'detail.html'
 })
 export class FilmDetailPage {
-  film;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public film;
+  public characters;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient) {
 
     this.film = this.navParams.get('film');
+    this.characters = new Array();
 
+    let urlCharacters = this.film['characters'];
+    urlCharacters.forEach((url) => {
+
+      this.http.get(url).subscribe(character => {
+        this.characters.push(character);
+      });
+
+    });
+
+    console.log(this.characters);
     console.log("FilmDetailPage", this.film);
   }
 
