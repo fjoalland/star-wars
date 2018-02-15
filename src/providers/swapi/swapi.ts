7caa3409827443481839c40bbb89af19d5b9d81a
Fabien Joalland <joalland.fabien@gmail.com>
2018-02-15 13:47:26 +0100

    Ajout de l'observeur detail dans le provider. Ajout de commentaires

--- src/providers/swapi/swapi.ts
+++ src/providers/swapi/swapi.ts
@@ -14,33 +14,40 @@ import {BehaviorSubject} from "rxjs/BehaviorSubject";
 export class SwapiProvider {
 
   private _films: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  private _filmDetails: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-  constructor(public http: HttpClient, public storage: Storage) {
-    console.log('Hello SwapiProvider Provider');
-  }
-
-
+  constructor(public http: HttpClient, public storage: Storage) { }
 
   getFilms() {
+    // On verifie si on a deja les films en cache
     this.storage.get('films').then((fimsLocal) => {
-      console.log(fimsLocal)
+
+      // Si on a les films en cache, on les retournent à travers un observable
       if(fimsLocal) {
         this._films.next(fimsLocal)
       } else {
+
+        // On appele SWAPI et on recupere les fims
         this.http.get('https://swapi.co/api/films/').subscribe((data) => {
           let films = data['results'];
           this._films.next(films)
-          this.storage.set('films', films).then((storageSet) => {
-            console.log(storageSet)
-          })
+          this.storage.set('films', films);
         });
       }
     })
+  }
 
+  getFilmDetails() {
+    
   }
 
 
-  get films(): Observable<string> {
+  get films(): BehaviorSubject<string> {
     return this._films;
+  }
+
+
+  get filmDetails(): BehaviorSubject<string> {
+    return this._filmDetails;
   }
 }
