2b2acb0869386c7ad4baa644cb662d366fb28527
Fabien Joalland <joalland.fabien@gmail.com>
2018-02-15 13:39:41 +0100

    Ajout d'une provider permettant de gerer SWAPI (creation d'un observeur)

--- src/pages/film/film.ts
+++ src/pages/film/film.ts
@@ -3,6 +3,7 @@ import {NavController, NavParams} from 'ionic-angular';
 import {HttpClient} from "@angular/common/http";
 import {FilmDetailPage} from "./detail/detail";
 import {SwapiProvider} from "../../providers/swapi/swapi";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'page-film',
@@ -10,11 +11,15 @@ import {SwapiProvider} from "../../providers/swapi/swapi";
 })
 export class FilmPage {
   results: string;
+  films$: Observable<string>;
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient, private swapi: SwapiProvider) {
-    this.results = swapi.getFilms().then((data) => {
-      console.log(data);
+    swapi.getFilms();
+    this.swapi.films.subscribe((data) => {
+      console.log(data)
       this.results = data;
-    });
+    })
 
     /*this.http.get('https://swapi.co/api/films/').subscribe((data) => {
       this.results = data['results'];
--- src/providers/swapi/swapi.ts
+++ src/providers/swapi/swapi.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs/Observable";
 import { Storage } from '@ionic/storage';
+import {Observable} from "rxjs/Observable";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 /*
   Generated class for the SwapiProvider provider.
@@ -12,6 +13,8 @@ import { Storage } from '@ionic/storage';
 @Injectable()
 export class SwapiProvider {
 
+  private _films: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
   constructor(public http: HttpClient, public storage: Storage) {
     console.log('Hello SwapiProvider Provider');
   }
@@ -19,17 +22,25 @@ export class SwapiProvider {
 
 
   getFilms() {
+    this.storage.get('films').then((fimsLocal) => {
+      console.log(fimsLocal)
+      if(fimsLocal) {
+        this._films.next(fimsLocal)
+      } else {
+        this.http.get('https://swapi.co/api/films/').subscribe((data) => {
+          let films = data['results'];
+          this._films.next(films)
+          this.storage.set('films', films).then((storageSet) => {
+            console.log(storageSet)
+          })
+        });
+      }
+    })
 
-    if(this.storage.get('films')){
-      //return this.storage.get('films');
-    }
-
-    this.http.get('https://swapi.co/api/films/').subscribe((data) => {
-      console.log('ok : ' + data);
-      let films = data['results'];
-      this.storage.set('films', films);
-      return films;
-    });
   }
 
+
+  get films(): Observable<string> {
+    return this._films;
+  }
 }
